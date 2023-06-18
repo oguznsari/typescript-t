10db156ab2b86751063f9c0f3755e78f7ccdae01
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-18 13:24:45 +0300

    adding typescript into existing app

---
+++ learning-typescript2/src/model.ts
@@ -0,0 +1,17 @@
+let displayName = "Jess's standing desk";
+let inventoryType = "furniture";
+let trackingNumber = "FD123455";
+let createDate = new Date();
+let originalCost = 425;
+
+function getInventoryItem(trackingNumber) {
+
+}
+
+function saveInventoryItem(item) {
+
+}
+
+let inventoryItem = getInventoryItem(trackingNumber);
+inventoryItem.createDate = new Date();
+saveInventoryItem(inventoryItem);
