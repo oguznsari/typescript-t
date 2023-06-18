c06118ee954a6d78e3ff1a9319ccab9130c693e4
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-18 16:07:08 +0300

    methods with .ts

--- oop-typescript/3.constructor-properties.ts
+++ oop-typescript/3.constructor-properties.ts
@@ -8,6 +8,12 @@ export class Message {
         this.message = message;
         this.isSent = false;
     }
+
+    priviewMessage(): string {
+        return this.message.slice(0, 10).concat('...')
+    }
 }
 
 const message = new Message("Hello", "World");
+
+message.priviewMessage();
