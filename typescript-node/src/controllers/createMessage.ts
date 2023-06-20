7959a4e00b52725ff76a0468a551d507fdcd9dd4
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-20 21:02:07 +0300

    namespaces & organising with modules

--- typescript-node/src/controllers/createMessage.ts
+++ typescript-node/src/controllers/createMessage.ts
@@ -9,3 +9,17 @@ export default class Messenger {
         return `Node and express server is running on port: ${this.port}`;
     }
 }
+
+// namespace Messagespace {
+//     export class Messenger {
+//         port: number;
+    
+//         constructor(port) {
+//             this.port = port;
+//         }
+    
+//         messagePrint() {
+//             return `Node and express server is running on port: ${this.port}`;
+//         }
+//     }
+// }
