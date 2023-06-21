0ed70eb89c7ef9a95a466f24e1714d9769272186
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-21 09:20:55 +0300

    other complex types | enum any void

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -7,13 +7,23 @@ function App() {
   // let firstValue: number[] = [2, 3, 56]; (obj?)
   // let firstValue: Array<string> = ['2', '3', '56'];
 
-  let aTuple: [string, number] = ['Manny', 10];
+  // tuple
+  // let aTuple: [string, number] = ['Manny', 10];
+  // enum   
+  // enum Codes { first = 1, second = 2 };
+  // any
+  let firstName: any = 'Manny';
+  // void
+  // const warning = (): void => {
+  //   console.log('Warning');
+  // }
+  // warning();
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          The value {aTuple} is of {typeof aTuple} type!
+          The value {firstName} is of {typeof firstName} type!
         </p>
       </header>
     </div>
