cf69307cb3caff9dbaa96736c31cdbd93755ff21
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-21 09:16:10 +0300

    tuple

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -5,13 +5,15 @@ function App() {
   // let firstValue: number = 10;
   // let firstValue: boolean = true;
   // let firstValue: number[] = [2, 3, 56]; (obj?)
-  let firstValue: Array<string> = ['2', '3', '56'];
+  // let firstValue: Array<string> = ['2', '3', '56'];
+
+  let aTuple: [string, number] = ['Manny', 10];
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          The value {firstValue} is of {typeof firstValue} type!
+          The value {aTuple} is of {typeof aTuple} type!
         </p>
       </header>
     </div>
