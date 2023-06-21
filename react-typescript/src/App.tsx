cc2fe52b701adf997069259a29ec93435196b009
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-06-21 09:14:10 +0300

    basic types

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,20 +1,18 @@
 import './App.css';
 
 function App() {
+  // let firstValue: string = 'Manny';
+  // let firstValue: number = 10;
+  // let firstValue: boolean = true;
+  // let firstValue: number[] = [2, 3, 56]; (obj?)
+  let firstValue: Array<string> = ['2', '3', '56'];
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The value {firstValue} is of {typeof firstValue} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
